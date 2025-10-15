f93993ccfeadc3fbfec8682978bccad49e2afb67
DevBilal17 <muhammadbilalmb0312@gmail.com>
2025-10-15 20:10:40 +0500

    Some Issues Facing for the Type Conversions

--- miniProject2/drawings.py
+++ miniProject2/drawings.py
@@ -8,6 +8,7 @@ image = cv2.imread(input_image)
 if image is  None:
     print("Error: Image is not found!")
 else:
+    image = cv2.resize(image,(300,300))
     print("\n1.Line on Image")
     print("\n2.Circle on Image")
     print("\n3.Rectangle on Image")
@@ -15,13 +16,26 @@ else:
     choice = int(input("\nWhat you want to perform? "))
     new_image = ''
     if(choice == 1):
-        pt1 = input("Enter Quardinates of Lines starting point in format (10,30) : ")
+        pt1 = input("Enter Quardinates of Lines starting point in format 10,30 : ")
         pt2 = input("Enter Quardinates of Lines Ending point in format (10,30) : ")
+        
+        pt1 = pt1.split(",")
+        pt2 = pt2.split(",")
+        new_pt1 = []
+        new_pt2 = []
+        for i in pt1:
+            new_pt1.append(int(i))
+        
+        for i in pt2:
+            new_pt2.append(int(i))
 
-        color = input("Enter color code in rgb format (12,345,234) : ")
+        new_pt1 = tuple(new_pt1)
+        new_pt2 = tuple(new_pt2)
+
+        
         thickness = int(input("Enter thickness of line e.g 1,2,3,... : "))
 
-        new_image = cv2.line(image,pt1,pt2,color,thickness)
+        new_image = cv2.line(image,new_pt1,new_pt2,(255,0,0),thickness)
 
     elif(choice == 2):
         center = input("Enter Center Quardinates for Circle to appear in format (10,30) : ")
@@ -56,7 +70,7 @@ else:
 
     y = input("What do you want to do with the image (1.Show | 2. Save) ? ")
     if(int(y)==1):
-        cv2.imshow(new_image)
+        cv2.imshow("New Image",new_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     elif(int(y)==1):
@@ -64,3 +78,12 @@ else:
         _, extension = os.path.splitext(input_image)
 
         cv2.imwrite(f"download${extension}",new_image)
+
+# f = input("Enter num : ")
+
+# n = f.split(",")
+# f = []
+# for i in n:
+#    f.append(int(i))
+
+# print(tuple(f))
