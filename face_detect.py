6b7479d21f56aadbb0de3f56bdaf80007c654f7d
DevBilal17 <muhammadbilalmb0312@gmail.com>
2025-10-19 11:20:00 +0500

    Rectangle Part Done

--- face_detect.py
+++ face_detect.py
@@ -11,5 +11,18 @@ while True:
     if not success:
         print("Image Not found")
         break
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.2,minNeighbors=2,minSize=(30,30),maxSize=(300,300))
 
+    for (x,y,w,h) in faces:
+        
+        cv2.rectangle(frame,(x,y),(x+w,h+y),(255,0,0),3)
     
+    cv2.imshow("Video",frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        print("Exit")
+        break
+
+cap.release()
+
+
